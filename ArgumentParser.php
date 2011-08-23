458a7889aa560b99517c3496604dd9a56f515434
Paul Matthews <pmatthews@ibuildings.com>
2011-08-23 21:02:39 +0100

    Writing code to complete seventh test

--- ArgumentParser.php
+++ ArgumentParser.php
@@ -8,6 +8,12 @@ class ArgumentParser
         foreach ($input as $item) {
             $realItem = str_replace('-', '', $item);
 
+            if (strstr($item, '=')) {
+                list($option, $value) = $this->getOptionAndValue($item);
+                $args[$option] = $value;
+                continue;
+            }
+
             $items = array($realItem);
             if (!strstr($item, '--')) {
                 $items = str_split($realItem);
@@ -19,5 +25,11 @@ class ArgumentParser
         }
 
         return $args;
+    }
+
+    protected function getOptionAndValue($string)
+    {
+        preg_match_all('/-+(\w+)="(\w+)"/', $string, $matches);
+        return array($matches[1][0], $matches[2][0]);
     }
 }
